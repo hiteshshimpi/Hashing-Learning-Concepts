532c5dc2608c3525652c1115234b114c7f83e1a3
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-03-05 17:54:16 +0530

    We are given an Array of Numbers. We have to find and print any Number with Maximum Frequency and Minimum Frequency.

---
+++ Hashing9.java
@@ -0,0 +1,41 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Hashing9 {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int[] arr = {3, 2, 3, 2, 4, 3};
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (!hash.containsKey(arr[i])) {
+                hash.put(arr[i], 1);
+            }
+            else
+            {
+                hash.put(arr[i], hash.get(arr[i]) + 1);
+
+            }
+        }
+        System.out.println(hash);
+        System.out.println("Max is  : " + max + " Min is :" + min);
+
+        Iterator<Map.Entry<Integer,Integer>> itr = hash.entrySet().iterator();
+            while (itr.hasNext())
+            {
+                Map.Entry<Integer,Integer> entry = itr.next();
+                if(entry.getValue()>max)
+                {
+                    max = entry.getValue();
+                }
+                else if(entry.getValue()<min)
+                {
+                    min = entry.getValue();
+                }
+            }
+        System.out.println("Max is  : " + max + " Min is :" + min);
+    }
+}
