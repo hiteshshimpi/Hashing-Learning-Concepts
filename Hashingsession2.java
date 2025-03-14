c7d59f15a917586de7abeb0a2cf0b6d5bd23d729
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-03-14 17:32:54 +0530

    // Problem :  count all (i,j) pairs such tthat b[i]+b[j] == k count such pairs Optimized apporach

--- Hashingsession2.java
+++ Hashingsession2.java
@@ -3,13 +3,16 @@ package Hashing;
 
 // Problem :  count all (i,j) pairs such tthat b[i]+b[j] == k count such pairs
 
+import java.util.HashMap;
+
 public class Hashingsession2 {
     public static void main(String[] args) {
 
-        int[] arr = {3, 2, 1, 2, 5};
-        int k = 4;
-        int count =bruteForceSol1(arr,k);
-        System.out.println(count);
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 5;
+        //int count =bruteForceSol1(arr,k);
+        //System.out.println(count);
+        hashMapSolution(arr,k);
 
     }
 
@@ -28,4 +31,22 @@ public class Hashingsession2 {
         }
         return count;
     }
+
+    public static void hashMapSolution(int [] arr, int k)
+    {
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        int count =0;
+        for(int i=0;i<arr.length;i++) {
+            int sum = Math.abs(arr[i]-k);
+            if(hash.containsKey(sum))
+            {
+                count++;
+            }
+            else {
+                hash.put(arr[i],1);
+            }
+        }
+        System.out.println(count);
+    }
+
 }
