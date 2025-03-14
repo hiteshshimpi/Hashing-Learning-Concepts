d17efd8887c8006a65ebc5be67a23a77a4165346
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-03-14 17:57:06 +0530

    // Count all i,j pairs where i<j and abs(b[i]-b[j]) = k [k>=0]

--- Hashingsession4.java
+++ Hashingsession4.java
@@ -1,18 +1,18 @@
 package Hashing;
 
 
-// Problem :  count all (i,j) pairs such tthat b[i]-b[j] == k count such pairs
+// Count all i,j pairs where i<j and abs(b[i]-b[j]) = k [k>=0]
 
 import java.util.HashMap;
 
-public class Hashingsession3 {
+public class Hashingsession4 {
     public static void main(String[] args) {
 
-        int[] arr = {1, 5, 2, 4, 1};
-        int k = 3;
+        int[] arr = {1, 5, 3, 4, 2};
+        int k = 2;
         int count =bruteForceSol1(arr,k);
         System.out.println(count);
-        hashMapSolution(arr,k);
+        hashMapSolutionsess4(arr,k);
 
     }
 
@@ -23,7 +23,7 @@ public class Hashingsession3 {
         {
             for (int j=i+1;j<arr.length;j++)
             {
-                    if(arr[i]-arr[j]==k)
+                    if( Math.abs(arr[i]-arr[j])==k)
                     {
                         count++;
                     }
@@ -32,7 +32,7 @@ public class Hashingsession3 {
         return count;
     }
 
-    public static void hashMapSolution(int [] arr, int k)
+    public static void hashMapSolutionsess4(int [] arr, int k)
     {
         HashMap<Integer,Integer> hash = new HashMap<>();
         int count =0;
