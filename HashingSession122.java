120f4a498cfd0f6ee855620c05472016dabe3554
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-03-19 20:05:44 +0530

    Two sum

---
+++ HashingSession122.java
@@ -0,0 +1,24 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class HashingSession122 {
+    public static void main(String[] args) {
+        int arr[] ={2,3,3};
+        int target = 6;
+        twoSum(arr,target);
+    }
+    public static void twoSum(int[] arr, int target){
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        for (int i=0;i<arr.length;i++)
+        {
+            int expectedKey = target -arr[i];
+            if(hash.containsKey(expectedKey))
+            {
+                System.out.println(hash.get(expectedKey)+" "+i);
+            }
+            hash.put(arr[i],i);
+        }
+
+    }
+}
